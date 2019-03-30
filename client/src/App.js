b218f0bd4d1a0870a8a63d8ef56a6e699dc19628
Alexander Augustin <alex@Alexanders-MacBook-Pro-2.local>
2019-03-30 08:45:41 -0400

    Made a table to display the languages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import Fetch from './Components/FetchData';
+import search from './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data: '',
+    }
+  }
+
+  eventHandler(event){
+    this.setState({
+      data:event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            WeCode
-          </a>
-        </header>
+
+        <h3>Search for a school</h3>
+        <div id={search['search-input']}>
+            <input type="text" onChange={this.eventHandler.bind(this)}/>
+            <p>{this.state.data}</p>
+        </div>
+        <Fetch/>
       </div>
     );
   }
--- client/src/Components/FetchData.js
+++ client/src/Components/FetchData.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Table } from 'react-bootstrap';
+import fetch from './FetchData.css';
 
 class FetchData extends Component {
 
@@ -8,21 +9,53 @@ class FetchData extends Component {
     user: null
   };
 
-  async componentDidMount() {
-    const url = "http://dummy.restapiexample.com/api/v1/employees";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({
-      user: data[0],
-      loading: false
-    });
-    console.log(data[0]);
-  }
+  // async componentDidMount() {
+  //   const url = "https://api.github.com/orgs/google/repos";
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   this.setState({
+  //     user: parseInt(data[0]),
+  //     loading: false
+  //   });
+  //   console.log(data[0]);
+  // }
 
   render() {
+
+    let java = 1040;
+    let cpp = 2048;
+    let python = 4440;
+    let swift = 728;
+
+
+    let total = java + cpp + python + swift;
+
+    java = (java/total) * 100;
+    cpp = (cpp/total) * 100;
+    python = (python/total) * 100;
+    swift = (swift/total) * 100;
+
+
     return (
       <div className="App">
-    {this.state.loading || !this.state.user ? (<div>Loading</div>) : (<div>{this.state.user.id}</div>)}
+        {this.state.loading || !this.state.user ? (<div>Loading</div>) : (<div>{this.state.user.id}</div>)}
+        
+        <Table striped bordered hover className='table'>
+          <tbody>
+            <tr>
+              <td>Java</td>
+              <td>80%</td>
+            </tr>
+            <tr>
+              <td>C++</td>
+              <td>50%</td>
+            </tr>
+            <tr>
+              <td>Python</td>
+              <td>40%</td>
+            </tr>
+          </tbody>
+        </Table>;
       </div>
     );
   }
