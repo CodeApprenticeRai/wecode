a70a76ce75997b24b477ce033ece017d815633a5
unknown <shoudou@outlook.com>
2019-03-29 23:39:42 -0500

    cleaning repo from pre-setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,7 +14,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            WeCode
           </a>
         </header>
       </div>
