22a321e25f03be525f02a6cfa5e60a066d2bd38d
Tare <tare.dgg@gmail.com>
2019-03-30 04:43:45 -0400

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            WeCode
-          </a>
-        </header>
+        <div className="container">
+          <div className="col-3">
+            <div className="row">
+            What Languages Do You Know?
+
+            Using GitHub data to objectively  visuzlize programming language competency
+            </div>
+            <div className="row">
+            1. Use the GitHub API to get information on a user's commit history.
+            2. Count the number of lines of code per file type.
+            3. Visualize it !
+            </div>
+          </div>
+          <div className="col">
+            <div className="row"></div>
+            <div className="row"></div>
+          </div>
+        <div>
       </div>
     );
   }
